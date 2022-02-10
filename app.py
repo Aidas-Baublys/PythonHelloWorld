8dfcd7b90622120a35a93640cca2e30b5881701c
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2022-02-10 18:19:16 +0200

    Make a simple weight converter

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-good_credit = False
-high_income = True
+print("Welcome to weight unit converter!")
 
-if not good_credit and high_income:
-    print("Cool")
+weight = float(input("What's your weight? Numbers only, please: "))
+unit_input = input("(K)g or (L)bs? Type letter, please: ")
+
+if unit_input.upper() == "K":
+    unit = "lbs"
+    weight //= 0.45
 else:
-    print("Nope")
+    unit = "kg"
+    weight *= 0.45
+
+print(f"Your weight is {weight}{unit}")
