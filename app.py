306e62f8fb2514b4408b2872b31699da22998986
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2022-02-10 18:55:51 +0200

    Update the number guessing game

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-secret_number = 4
+secret_number = 1
+tries = 3
 
-guess = int(input("Guess a number from 1 to 9: "))
+print("GUESS THE SECRET NUMBER" + "!" * secret_number)
+print(f"{tries} tries left")
+guess = int(input("Guess a number from 0 to 9: "))
 
-while guess != secret_number:
-    guess = int(input("Guess a number from 1 to 9: "))
+while guess != secret_number and tries > 1:
+    tries -= 1
+    print(f"{tries} left")
+    guess = int(input("Guess a number from 0 to 9: "))
 
-print("Bravo!")
+if guess == secret_number:
+    print("Bravo!")
+else:
+    print(f"Loser! It was {secret_number}")
